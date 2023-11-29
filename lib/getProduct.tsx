013eb5caea58fafdde4883c81fb7d2ced1eaded8
Toms Freimanis <Tomsfreimans21@gmail.com>
2023-11-29 10:49:37 +0200

    some error handling

--- lib/getProduct.tsx
+++ lib/getProduct.tsx
@@ -1,15 +1,17 @@
 export default async function getProduct(id: string) {
-    try {
-      const res = await fetch(`https://run.mocky.io/v3/b54fe93f-f5a1-426b-a76c-e43d246901fd/${id}`);
-  
-      if (!res.ok) {
-        throw new Error(`Failed to fetch product. Status: ${res.status}`);
-      }
-  
-      return res.json();
-    } catch (error) {
-      // Instead of logging the error, throw it again
-      throw error;
+  try {
+    const res = await fetch(`https://run.mocky.io/v3/b54fe93f-f5a1-426b-a76c-e43d246901fd/${id}`);
+
+    if (!res.ok) {
+      throw new Error(`Failed to fetch product. Status: ${res.status}`);
     }
+
+    return res.json();
+  } catch (error) {
+    // Log the error for debugging purposes
+    console.error("Error fetching product:", error);
+    
+    // Rethrow the error to provide more details in the console
+    throw error;
   }
-  
+}
